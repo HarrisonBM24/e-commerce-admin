0da6cc846d42896162d1c1e5695e8f5d9e113d0e
HarrisonBM24 <mharry2404@gmail.com>
2024-04-30 22:30:44 -0400

    feat: Importing necessary AWS SDK modules for S3 interaction, Setting up S3 client with appropriate credentials and region, Iterating through each file and uploading it to S3 with ACL set to public-read, Generating unique filenames for uploaded files and Returning S3 URLs for each uploaded file in the API response

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,4 +1,9 @@
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import fs from "fs";
+import mime from "mime-types";
 import multiparty from "multiparty";
+
+const bucketName = "hbm-ecommerce";
 
 export default async function handle(req, res) {
   const form = new multiparty.Form();
@@ -12,7 +17,34 @@ export default async function handle(req, res) {
 
   console.log("length", files.file.length);
 
-  res.json("ok");
+  const client = new S3Client({
+    region: "us-east-2",
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    },
+  });
+
+  const links = [];
+  for (const file of files.file) {
+    const ext = file.originalFilename.split(".").pop();
+    const newFilename = Date.now() + "." + ext;
+    console.log({ ext, file });
+
+    await client.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: newFilename,
+        Body: fs.readFileSync(file.path),
+        ACL: "public-read",
+        ContentType: mime.lookup(file.path),
+      })
+    );
+    const link = `https://${bucketName}.s3.amazonaws.com/${newFilename}`;
+    links.push(link);
+  }
+
+  res.json({ links });
 }
 
 export const config = {
