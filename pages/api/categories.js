2d959c1830db7086b075824d06bb52a11442d091
HarrisonBM24 <mharry2404@gmail.com>
2024-05-05 22:33:45 -0400

    added method put and delete to handle theses req

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -15,4 +15,24 @@ export default async function handle(req, res) {
   if (method === "GET") {
     res.json(await Category.find().populate("parent"));
   }
+
+  if (method === "PUT") {
+    const { name, parentCategory, _id } = req.body;
+    const categoryDoc = await Category.updateOne(
+      { _id },
+      {
+        name,
+        parent: parentCategory,
+      }
+    );
+
+    res.json(categoryDoc);
+  }
+
+  if (method === "DELETE") {
+    const { _id } = req.query;
+
+    await Category.deleteOne({ _id });
+    res.json("ok");
+  }
 }
