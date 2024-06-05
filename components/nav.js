05c386406df759b21414f1e3d116c458b42b5cb5
HarrisonBM24 <mharry2404@gmail.com>
2024-06-05 10:41:05 -0400

    Feat: Update Nav component with responsive design and logout functionality
    - Added signOut functionality and logout button.
    - Updated styles for active/inactive links and icons.
    - Implemented responsive design for sidebar navigation.
    - Replaced static logo with Logo component.

--- components/nav.js
+++ components/nav.js
@@ -1,31 +1,32 @@
+import { signOut } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import Logo from "./Logo";
 
-export default function Nav() {
+export default function Nav({ show }) {
   const inactiveLink = "flex gap-1 p-1";
-  const activeLink = inactiveLink + " bg-white text-blue-900 rounded-l-lg";
+  const activeLink = inactiveLink + " bg-highlight text-black rounded-md";
+  const inactiveIcon = "w-6 h-6";
+  const activeIcon = inactiveIcon + " text-primary";
   const router = useRouter();
   const { pathname } = router;
 
+  async function logout() {
+    await router.push("/");
+    await signOut();
+  }
+
   return (
-    <aside className="p-4 pr-0 text-white">
-      <Link href={"/"} className="flex gap-1 mb-4 mr-4">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
-          />
-        </svg>
-        <span>Ecommerce-Admin</span>
-      </Link>
+    <aside
+      className={
+        (show ? "left-0" : "-left-full") +
+        " top-0 text-gray-500 p-4 fixed w-full bg-bgGray h-full md:static md:w-auto transition-all"
+      }
+    >
+      <div className="mb-4 mr-4">
+        <Logo />
+      </div>
+
       <nav className="flex flex-col gap-2">
         <Link
           href={"/"}
@@ -37,7 +38,7 @@ export default function Nav() {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="w-6 h-6"
+            className={pathname === "/" ? activeIcon : inactiveIcon}
           >
             <path
               strokeLinecap="round"
@@ -57,7 +58,7 @@ export default function Nav() {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="w-6 h-6"
+            className={pathname === "/products" ? activeIcon : inactiveIcon}
           >
             <path
               strokeLinecap="round"
@@ -79,7 +80,7 @@ export default function Nav() {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="w-6 h-6"
+            className={pathname === "/categories" ? activeIcon : inactiveIcon}
           >
             <path
               strokeLinecap="round"
@@ -99,7 +100,7 @@ export default function Nav() {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="w-6 h-6"
+            className={pathname === "/orders" ? activeIcon : inactiveIcon}
           >
             <path
               strokeLinecap="round"
@@ -119,7 +120,7 @@ export default function Nav() {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="w-6 h-6"
+            className={pathname === "/settings" ? activeIcon : inactiveIcon}
           >
             <path
               strokeLinecap="round"
@@ -134,6 +135,23 @@ export default function Nav() {
           </svg>
           Settings
         </Link>
+        <button onClick={logout} className={inactiveLink}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
+            />
+          </svg>
+          Logout
+        </button>
       </nav>
     </aside>
   );
