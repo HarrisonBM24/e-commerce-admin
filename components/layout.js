92beca036ad5dc4cf44298e7aa40c408dea62fd3
HarrisonBM24 <mharry2404@gmail.com>
2024-06-05 10:38:33 -0400

    Feat: Enhanced Layout component with responsive navigation
    - Added useState to manage navigation visibility
    - Integrated Logo component
    - Updated styling for responsive design

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,15 @@
 import Nav from "@/components/Nav";
 import { signIn, useSession } from "next-auth/react";
+import { useState } from "react";
+import Logo from "./Logo";
 
 export default function Layout({ children }) {
+  const [showNav, setShowNav] = useState(false);
   const { data: session } = useSession();
+
   if (!session) {
     return (
-      <div className="flex items-center w-screen h-screen bg-blue-900">
+      <div className="flex items-center w-screen h-screen bg-bgGray">
         <div className="w-full text-center">
           <button
             onClick={() => signIn("google")}
@@ -18,10 +22,33 @@ export default function Layout({ children }) {
     );
   }
   return (
-    <div className="flex min-h-screen bg-blue-900">
-      <Nav />
-      <div className="flex-grow p-4 mt-2 mr-2 bg-white rounded-lg">
-        {children}
+    <div className="min-h-screen bg-bgGray">
+      <div className="flex items-center p-4 md:hidden">
+        <button onClick={() => setShowNav(true)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </button>
+        <div className="flex justify-center mr-6 grow">
+          <Logo />
+        </div>
+      </div>
+      <div className="flex">
+        <Nav show={showNav} />
+        <div className="flex-grow p-4 mt-2 mr-2 bg-white rounded-lg">
+          {children}
+        </div>
       </div>
     </div>
   );
